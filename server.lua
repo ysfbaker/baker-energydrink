838ff6d6d17051e66463d9d68f372cd6aab81f9f
Yusuf BAKER <108697153+ysfbaker@users.noreply.github.com>
2024-07-04 21:48:52 +0300

    server.lua

--- server.lua
+++ server.lua
@@ -6,10 +6,3 @@ QBCore.Functions.CreateUseableItem('enerjiicecegi', function(source)
     TriggerClientEvent('baker:useEnergyDrink', src)
     Player.Functions.RemoveItem('enerjiicecegi', 1)
 end)
-
--- QBCore.Functions.CreateUseableItem('enerjiicecegi2', function(source)
---     local src = source
---     local Player = QBCore.Functions.GetPlayer(src)
---     TriggerClientEvent('baker:useEnergyDrink', src)
---     Player.Functions.RemoveItem('enerjiicecegi2', 1)
--- end)
